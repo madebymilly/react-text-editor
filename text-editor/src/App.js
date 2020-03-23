fadb222dfe82a2c12c9c7de63569b8d55566702c
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2020-03-23 10:17:19 +0100

    Static version & basic setup text-editor with styled-components

---
+++ text-editor/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Markup, Editor, Container, Column, Row, RuleInput, RuleLabel, StyleInput, Button, Document } from './styled.js';
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Column>
+          <Button>
+            New Rule
+          </Button>
+        </Column>
+        <Column>
+          <Button>
+            Random Text
+          </Button>
+          <Document>
+            <Editor/>
+            <Markup />
+          </Document>
+        </Column>
+      </Container>
+    </>
+  );
+}
+
+export default App;
